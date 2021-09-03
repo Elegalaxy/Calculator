e71ccc35464cf90dddcefd3f73232979eaae0fba
ZeKai Chong <yokai.czk@gmail.com>
2021-09-03 15:05:37 +0930

    digit check
    add digit check to prevent calculator from breaking

--- main.cpp
+++ main.cpp
@@ -9,10 +9,15 @@ using namespace std;
 void print(string s){
     cout << "------------" << endl;
     cout << '|';
-    for(int i = 0; i < 10-s.length(); i++){
-        cout << " ";
-    }
-    cout << s << '|' << endl;
+
+    if(s.length() <= 10){ //Check if the result is over 10 digits
+        for(int i = 0; i < 10-s.length(); i++){
+            cout << " ";
+        }
+        cout << s;
+    }else cout << "9999999999";
+
+    cout << "|" << endl;
     cout << "------------" << endl;
     cout << "| 1 2 3 |+ |" << endl;
     cout << "| 4 5 6 |- |" << endl;
@@ -24,15 +29,18 @@ void print(string s){
 string parse(string s){
     int a, b; //Integer of number
     string as = "", bs = ""; //String of number
-    int opt = s.length();
+    int opt = s.length(); //Index of operator
 
+    //Get index of operator
     for(int i = 0; i < s.length(); i++)
         if(s[i] < '0')
             opt = i;
     
+    //Return whole number if there is no operator
     if(opt == s.length())
         return s;
 
+    //Separate the string to numbers using index of operator
     as = s.substr(0, opt);
     bs = s.substr(opt+1, s.length()-opt-1);
 
@@ -51,6 +59,7 @@ string parse(string s){
         default:
             return as;
     }
+
     return "";
 }
 
@@ -62,9 +71,12 @@ int main(){
     while(1){
         cout << "Enter your calculation with no space and positive number (QUIT to exit): ";
         cin >> s;
+
+        //Quit
         if(s == "QUIT")
             break;
 
+        //Check valid input
         for(char i:s){
             if((i >= '0' && i <= '9') || i == '+' || i == '-' || i == '*' || i == '/'){
                 continue;
